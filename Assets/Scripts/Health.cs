6208795c5eb1dabeea7d5837c4f34d6c1a5c025a
Baglan Tolebay <baglantulebay@gmail.com>
2022-10-31 08:22:33 +0600

    Player death made. Revive system developing started

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,13 +5,42 @@ using Photon.Pun;
 using TMPro;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviourPun
 {
     public Image fillImage;
+    public float health = 1;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public BoxCollider2D collider;
+
+    public void CheckHealth() {
+        if (photonView.IsMine && health <= 0) {
+            this.GetComponent<PhotonView>().RPC("death",RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    public void death() {
+        rb.gravityScale = 0;
+        collider.enabled = false;
+        sr.enabled = false;
+    
+    }
+
+    [PunRPC]
+    public void Revive() {
+        rb.gravityScale = 1;
+        collider.enabled = true;
+        sr.enabled = true;
+    
+    }
 
     [PunRPC]
     public void HealthUpdate(float damage) {
-        fillImage.fillAmount -= damage; 
+        fillImage.fillAmount -= damage;
+        health = fillImage.fillAmount;
+        CheckHealth();
     }
 
 }
